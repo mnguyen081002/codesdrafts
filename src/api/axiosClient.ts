a7b019c0b00abf8de3273f036fda100c79955978
Lang Minh Nguyen <3langn@gmail.com>
2022-12-28 13:21:16 +0700

    [update] api url

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://codesmooth-api-production.up.railway.app';
 
 const ApiClient = () => {
   const instance = axios.create({
