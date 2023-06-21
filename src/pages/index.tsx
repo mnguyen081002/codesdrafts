09662de4ccdbe77761bbcfd162a1d81875cb819a
Lang Minh Nguyên <3langn@gmail.com>
2023-06-21 12:26:39 +0700

    [FIX] Snackbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,26 @@ import { useSession } from 'next-auth/react';
 import ProtectedPage from '@/components/auth/ProtectedPage';
 
 import Teach from '../components/Teach';
+import { Meta } from '../layouts/Meta';
+import { Main } from '../templates/Main';
 
 const Index = () => {
   const session = useSession();
   console.log(session.data?.expires);
   return (
     <div>
-      {/* <Main
+      <Main
         meta={
           <Meta
             title="Next.js Boilerplate Presentation"
             description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
           />
         }
-      > */}
-      <ProtectedPage>
-        <Teach />
-      </ProtectedPage>
-      {/* </Main> */}
+      >
+        <ProtectedPage>
+          <Teach />
+        </ProtectedPage>
+      </Main>
     </div>
   );
 };
