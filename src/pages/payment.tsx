402aa8c457881de59a2144c642a7f37a8f2312fb
Lang Minh Nguyên <3langn@gmail.com>
2023-07-17 20:41:18 +0700

    [MF] Payment Page

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -117,50 +117,47 @@ function MainRight() {
   return (
     <div className="flex flex-col gap-[15px] rounded-md bg-white p-4 shadow-md">
       <img src="/images/course/Thumnail.png" alt="" />
-      <div className="flex flex-col gap-[30px] py-2">
-        <div className="flex flex-col gap-[50px]">
-          <div>
-            <p className="font-lexend-deca text-2xl font-semibold leading-6 text-black">
-              Master Design System In Figma
-            </p>
-            <div className="flex h-full gap-[9px]">
-              <Avatar h={45} w={45} />
-              <div className="flex h-[43px] flex-col justify-center">
-                <p className="font-inter text-base font-medium leading-[14px] text-[#141414] opacity-[87%]">
-                  Minh Nguyên
-                </p>
-                <p className="font-inter text-[13px] font-normal leading-[20px] text-light-text-sencondary">
-                  Google Expert
-                </p>
-              </div>
+      <div className="flex h-full flex-col justify-between">
+        <div className="flex flex-col gap-[12px]">
+          <p className="font-lexend-deca text-2xl font-semibold leading-6 text-black">
+            Master Design System In Figma
+          </p>
+          <div className="flex h-full gap-[9px]">
+            <Avatar h={45} w={45} />
+            <div className="flex h-[43px] flex-col justify-center">
+              <p className="font-inter text-base font-medium leading-[14px] text-[#141414] opacity-[87%]">
+                Minh Nguyên
+              </p>
+              <p className="font-inter text-[13px] font-normal leading-[20px] text-light-text-sencondary">
+                Google Expert
+              </p>
             </div>
           </div>
-
-          <div className="flex flex-col gap-[10px]">
-            <div className="flex justify-between">
-              <p className="font-lexend-deca text-base font-medium text-light-text-sencondary">
-                Giá bán:
-              </p>
-              <p className="font-lexend-deca text-[22px] font-medium leading-6 text-black">
-                150.000 VNĐ
-              </p>
-            </div>
-            <div className="flex flex-col justify-center gap-[6px] border-t border-light-border pt-[15px]">
-              <p className="font-lexend-deca  text-[16px] font-medium text-light-text-sencondary">
-                Mã khuyến mãi
-              </p>
-              <div className="flex w-fit items-center gap-[10px]">
-                <input
-                  type="text"
-                  className="h-[45px] w-[200px]  rounded-md bg-white px-[20px] placeholder-[#4C4E64] placeholder-opacity-[60%]"
-                  placeholder="Nhập mã khuyến mãi"
-                />
-                <PrimaryOutlineButton
-                  textClassName="text-[12px] leading-[20px]"
-                  className="px-[10px] py-[6px]"
-                  text="Áp Dụng"
-                />
-              </div>
+        </div>
+        <div className="flex flex-col gap-[10px]">
+          <div className="flex justify-between">
+            <p className="font-lexend-deca text-base font-medium text-light-text-sencondary">
+              Giá bán:
+            </p>
+            <p className="font-lexend-deca text-[22px] font-medium leading-6 text-black">
+              150.000 VNĐ
+            </p>
+          </div>
+          <div className="flex flex-col justify-center gap-[6px] border-t border-light-border pt-[15px]">
+            <p className="font-lexend-deca  text-[16px] font-medium text-light-text-sencondary">
+              Mã khuyến mãi
+            </p>
+            <div className="flex w-fit items-center gap-[10px]">
+              <input
+                type="text"
+                className="h-[45px] w-[200px]  rounded-md bg-white px-[20px] placeholder-[#4C4E64] placeholder-opacity-[60%]"
+                placeholder="Nhập mã khuyến mãi"
+              />
+              <PrimaryOutlineButton
+                textClassName="text-[12px] leading-[20px]"
+                className="px-[10px] py-[6px]"
+                text="Áp Dụng"
+              />
             </div>
           </div>
         </div>
@@ -236,8 +233,8 @@ const Checkout = () => {
           )}
         </div>
       ) : (
-        <div className="flex h-fit w-full items-center justify-center py-[60px]">
-          <div className="flex h-full w-fit gap-[40px] rounded-md border-2 border-light-border px-[50px] py-[40px]">
+        <div className="flex w-full justify-center py-[80px]">
+          <div className="flex w-fit items-center gap-[40px] rounded-md px-[50px] py-[40px] shadow-md">
             <div className="flex h-[576px] gap-[50px]">
               <MainLeft></MainLeft>
               <MainRight></MainRight>
