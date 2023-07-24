8c892a5d39367d735b42cda8ea771c82bc89ce00
thanhbk113 <thanhbk113@gmail.com>
2023-07-25 00:50:48 +0700

    improve seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -56,24 +56,23 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
           }}
         >
           <Head>
-            <title> CodeDrafts - Học lập trình thật dễ</title>
+            <title>CodeDrafts - Học lập trình thật dễ</title>
             <meta charSet="utf-8" />
             <link rel="canonical" href="http://codedrafts.com" />
             <meta
               name="description"
-              content="Website CodeDrafts cung cấp các khóa học lập trình chuyên nghiệp và hữu ích về reactjs, nextjs, nodejs, golang, frontend, backend, và devops."
+              content="CodeDrafts là nền tảng học lập trình hàng đầu cung cấp các khóa học chất lượng cao về lập trình web. Tại CodeDrafts, bạn sẽ tìm thấy những khóa học chuyên sâu về reactjs, nextjs, nodejs, golang và devops, giúp bạn nắm vững từng khía cạnh trong công nghệ này. Hãy tham gia vào các khóa học frontend và backend tại CodeDrafts để trở thành một lập trình viên chuyên nghiệp và sáng tạo những ứng dụng web đẹp và mạnh mẽ.Nắm vững công nghệ mới nhất, xây dựng dự án thực tế, và được hướng dẫn bởi các chuyên gia hàng đầu trong ngành.Với chúng tôi, học lập trình chưa bao giờ dễ dàng và thú vị đến thế!"
             />
 
             <meta
               name="keywords"
-              content="học lập trình, pro lập trình, lập trình, khóa học lập trình, reactjs, nextjs, nodejs, golang, frontend, backend, devops"
-            />
-            <meta
-              name="keywords"
-              content="dạy lập trình, học lập trình, dạy lập trình miễn phí, học lập trình miễn phí, học lập trình online, học lập trình từ cơ bản đến nâng cao, khóa học lập trình, học lập trình trực tuyến"
+              content="học lập trình, pro lập trình, lập trình, khóa học lập trình, reactjs, nextjs, nodejs, golang, frontend, backend, devops, dạy lập trình, dạy lập trình miễn phí, học lập trình online, học lập trình từ cơ bản đến nâng cao, khóa học lập trình trực tuyến"
             />
             <meta name="robots" content="index, follow" />
+            <meta name="author" content="CodeDrafts Team" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
           </Head>
+
           <ToastContainer limit={1} />
           <Container>{getLayout(<Component {...pageProps} />)}</Container>
         </MantineProvider>
