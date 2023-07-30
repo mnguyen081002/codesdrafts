290a033d810233290f93fc8fe3e67b02b85223a2
Lang Minh Nguyên <3langn@gmail.com>
2023-07-31 01:11:40 +0700

    fix: unload img

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ module.exports = withBundleAnalyzer({
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: false,
   env: {
     HOST: process.env.HOST,
